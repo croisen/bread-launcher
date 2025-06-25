371417481232fe212e157d819725f8534cc3628b
croisen <andrewjames.git.gan@gmail.com>
2025-06-25 15:08:23 +0800

    Asset path

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -1,3 +1,4 @@
-// Change the '/' to a '\\' if you're compiling on windows
+// Comment the first one and uncomment the second one if compiling in Windows
 // I've left this here since uhhh I'm compiling using mingw
 pub static ICON_0: &'static [u8] = include_bytes!("icons/0-mc-logo.png");
+// pub static ICON_0: &'static [u8] = include_bytes!("icons\\0-mc-logo.png");
